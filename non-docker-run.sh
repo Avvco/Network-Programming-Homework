f5bd5fa1741322ab53123c3fab4ef4b4882f602b
Avvco <leei910110@gmail.com>
2023-03-14 17:26:10 +0800

    modify to use fork and pipe

---
+++ non-docker-run.sh
@@ -0,0 +1,4 @@
+rm -f ./object/out.out
+gcc ./src/main.c -o ./object/out.out 
+./object/out.out
+rm -f ./object/out.out
--- src/main.c
+++ src/main.c
@@ -6,9 +6,9 @@
 #include <dirent.h> 
 #include <ctype.h>
 
-#define die(e) do { fprintf(stderr, "stderr: %s\n", e); exit(EXIT_FAILURE); } while (0);
-#define MAX_COMMANDS_SIZE 5000
 #define PREVIOUS_COMMAND_OUTPUT_SIZE 32768
+#define die(e) do { fprintf(stderr, "%s\n", e); exit(EXIT_FAILURE); } while (0);
+
 char existBin[100][100];
 int existBinCount = 0;
 char previousCommandOutput[PREVIOUS_COMMAND_OUTPUT_SIZE];
@@ -16,38 +16,8 @@ char previousCommandOutput[PREVIOUS_COMMAND_OUTPUT_SIZE];
 // use for number pipe
 char savedCommandOutput[PREVIOUS_COMMAND_OUTPUT_SIZE];
 
-int run_bash(const char *command) {
-  int link[2];
-  pid_t pid;
-  char foo[4096 + 1];
-
-  if (pipe(link) == -1)
-    die("pipe");
-
-   if ((pid = fork()) == -1)
-    die("fork");
-
-  if(pid == 0) {
-    dup2 (link[1], STDOUT_FILENO);
-    close(link[0]);
-    close(link[1]);
-    execl("/bin/bash", "bash", "-c", command, (char *)0);
-    die("execl");
-  } else {
-    close(link[1]);
-    int nbytes = 0;
-    memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
-    while(0 != (nbytes = read(link[0], foo, sizeof(foo)))) {
-      //printf("%.*s\n", nbytes, foo);
-      strcat(previousCommandOutput, foo);
-      memset(foo, 0, 4096);
-    }
-
-    // remove the last line break
-    if(strcmp(&previousCommandOutput[strlen(previousCommandOutput) - 1], "\n") == 0) previousCommandOutput[strlen(previousCommandOutput) - 1] = '\0';
-    wait(NULL);
-  }
-}
+#include "../include/commandParse.c"
+#include "../include/runCommand.c"
 
 void init() {
   printf("Welcome to the shell!\n");
@@ -73,63 +43,22 @@ void init() {
   setenv("PATH", "/bin:./bin", 1);
 }
 
-/**
- * @brief trim the command by remove space and line breaks
- * 
- * @param str command to trim
- */
-void trim_command(char *str) {
-  // remove line breaks
-  strtok(str, "\n");
-
-  char *dst = str;
-  for (; *str; ++str) {
-    *dst++ = *str;
-    if (isspace(*str)) {
-      do ++str; 
-      while (isspace(*str));
-      --str;
-    }
-  }
-  *dst = 0;
-}
-
-/**
- * @brief split the command with space
- * 
- * @param command command to split
- * @param args the array to store the splited command
- * @return the number of splited command
- */
-int command_parse(char *command, char **args) {
-  int argc = 0;
-  char *arg = malloc(MAX_COMMANDS_SIZE * sizeof(char));
-  char *delim = " ";
-  arg = strtok(command, delim);
-  while (arg != NULL) {
-    args[argc++] = arg;
-    arg = strtok(NULL, delim);
-  }
-  args[argc] = NULL;
-  return argc;
-}
-
 int main(int argc, char **argv, char **envp) {
   char command[MAX_COMMANDS_SIZE];
   char quit[] = "quit";
   char _exit[] = "exit";
   char _setenv[] = "setenv";
   char printenv[] = "printenv";
-  char command_not_found[] = "command not found";
 
   int pipeCounter = 0;
 
   init();
   while (1) {
-    char **splitedCommand = malloc(MAX_COMMANDS_SIZE * sizeof(char *));;
+    char **splitedCommand = malloc(MAX_COMMANDS_SIZE * sizeof(char *));
     int isInternalCommand = 0;
     int piped = 0;
     printf("%s", "% ");
+    
     if(fgets(command, MAX_COMMANDS_SIZE, stdin) == NULL) {
       printf("\n");
       return 0;
@@ -189,15 +118,12 @@ int main(int argc, char **argv, char **envp) {
       }
 
       /**
-       * Build the command to the following format:
-       * echo -e "previousCommandOutput" | _currentCommand
+       * Build the command
        * 
        * Pipe all previous command output to the current command,
        * it doesn't take effect if there is no previous command output or the current command is not accept input.
        */
-      char currentCommand[1024];
-      strcpy(currentCommand, "echo -e \"");
-      // printf("pipeCounter: %d\n", pipeCounter);
+      char *currentCommand = malloc(MAX_COMMANDS_SIZE * sizeof(char));
 
       /**
        * Pipe counter:
@@ -206,16 +132,10 @@ int main(int argc, char **argv, char **envp) {
        * >1: command output is queued to be piped
        */
       if(pipeCounter <= 0) {
-        strcat(currentCommand, "");
         pipeCounter = 0;
-      }else if(pipeCounter > 1) {
-        strcat(currentCommand, "");
-        pipeCounter--;
-      }else if(pipeCounter == 1) {
-        strcat(currentCommand, previousCommandOutput);
+      }else if(pipeCounter >= 1) {
         pipeCounter--;
       }
-      strcat(currentCommand, "\" | ");
 
       /**
        * Add "./bin/" in front of the command if it is an internal command and 
@@ -229,7 +149,7 @@ int main(int argc, char **argv, char **envp) {
       }
 
       // printf("currentCommand: %s\n", currentCommand);
-      run_bash(currentCommand);
+      run_command(currentCommand);
 
 
       /**
@@ -239,10 +159,8 @@ int main(int argc, char **argv, char **envp) {
       if(splitedCommand[i] != NULL) {
         char *pipCount = strstr(splitedCommand[i], "|");
         if(pipCount != NULL) {
-          // printf("pipCount: %s\n", pipCount);
           if(strlen(pipCount) > 1) {
             pipeCounter = atoi(&pipCount[1]);
-            // printf("pipeCounter: %d\n", pipeCounter);
           }
           else pipeCounter = 1;
           memset(savedCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
@@ -262,6 +180,7 @@ int main(int argc, char **argv, char **envp) {
      * 1: next command is piped
      * >1: ignore
      */
+    // printf("previousCommandOutput: %d\n", strlen(previousCommandOutput));
     if(strlen(previousCommandOutput) > 0) {
       if(pipeCounter == 0) printf("%s\n", previousCommandOutput);
       if(pipeCounter != 0 && !piped) printf("%s\n", previousCommandOutput);
