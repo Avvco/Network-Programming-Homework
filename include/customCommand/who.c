ede23557c062951db00591d3ba7da62fef80750a
Avvco <leei910110@gmail.com>
2023-05-24 14:26:57 +0800

    fix index error

--- include/customCommand/who.c
+++ include/customCommand/who.c
@@ -17,12 +17,12 @@ void* whoProcessCommand(void *commandRequirement) {
   for(int i = 0 ; i < MAX_CLIENTS ; i++) {
     for(int j = 0 ; j < MAX_CLIENTS ; j++) {
       if(session[j].assignedId == i) {
-        char *currentStr = malloc(256 * sizeof(char));
+        char *currentStr = malloc(512 * sizeof(char));
         char *port = malloc(16 * sizeof(char));
         snprintf(port, 16,"%d", session[j].port);
         char *ipAndPort = malloc(32 * sizeof(char));
         snprintf(ipAndPort, 32, "%s:%s", session[j].ip, port);
-        snprintf(currentStr, 256,"%-8d%-20s%-20s%-20s\n", i, getNameByUserId(session[i].loggedInUserId), ipAndPort, (current -> currentId == i) ? "<- (ME)" : "");
+        snprintf(currentStr, 512,"%-8d%-20s%-20s%-20s\n", i, getNameByUserId(session[j].loggedInUserId), ipAndPort, (current -> currentId == i) ? "<- (ME)" : "");
         strcat(returnStr, currentStr);
         free(currentStr);
         free(ipAndPort);
